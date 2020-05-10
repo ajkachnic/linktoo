88099cfaa073160f498fd7222660b15d51c15894
Andrew Kachnic <ajkachnic@protonmail.com>
2020-05-10 09:47:54 -0400

    :bug: Fixed OAuth errors

--- api/auth/github/callback.js
+++ api/auth/github/callback.js
@@ -6,17 +6,11 @@ import passport from '../../../utils/passport'
 const handler = nextConnect()
 
 handler.use(middleware)
-handler.use(
-  passport.authenticate('github', {
-    failWithError: true,
-    failureRedirect: '/error',
-    successRedirect: '/app'
-  })
-)
-// handler.get((req, res) => {
-//   res.statusCode = 301
-//   res.setHeader('Location', '/app')
-//   res.end()
-// })
+handler.use(passport.authenticate('github'))
+handler.get((req, res) => {
+  res.statusCode = 301
+  res.setHeader('Location', '/app')
+  res.end()
+})
 
 export default handler
